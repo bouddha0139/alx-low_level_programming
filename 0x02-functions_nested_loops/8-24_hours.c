61c176af2b245b50aac98f435c25e82306c13ef6
Abdessamad <Abdoubsabsa013@gmail.com>
2023-06-21 07:42:25 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * jack_bauer - imprime chaque minute de la journée de Jack Bauer
+ * de 00:00 à 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 60)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
