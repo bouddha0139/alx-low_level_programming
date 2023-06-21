cff85cbcde0e486e40521bbcd554bd4d58ca4fef
Abdessamad <Abdoubsabsa013@gmail.com>
2023-06-21 07:50:27 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - ajoute deux entiers et renvoie le résultat
+ * @a: entier à ajouter à b
+ * @b: entier à ajouter à a
+ * Return: somme de a et b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
