aa9c4e74e59076c3bfd83fe0b596b311343da681
Abdessamad <Abdoubsabsa013@gmail.com>
2023-06-21 07:36:14 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - calcule la valeur absolue d'un entier
+ * @n: l'entier à vérifier
+ *
+ * Return: la valeur absolue de l'entier
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
+
